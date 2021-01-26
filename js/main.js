564339a527f8bb82a08f061a2c397915724d6ef6
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-01-26 22:29:17 +0100

    Disable click events if win or a draw is achieved

--- js/main.js
+++ js/main.js
@@ -3,25 +3,33 @@ import Gameboard from './tictactoe/Gameboard.js';
 let board = new Gameboard();
 let fields = document.querySelector('#gameboard').children
 
-for (let i = 0; i < fields.length; i++) {
-    fields.item(i).addEventListener('click', e => {
-        if (board.setFieldValue(e.target.id)) {
-            let gameStatus = board.checkWin()
-            if (gameStatus === '') {
-                board.setNextPlayer();
-            } else if (gameStatus === "draw") {
-                // remove event listeners from
-                // write a message of a draw to a HTML element
-                console.log("It's a draw.");
-            } else {
-                // remove event listeners from
-                // write a message of the winner to a HTML element
-                console.log('Player ' + board.getNextPlayer().symbol + ' wins');
-            }
-            board.printGameboard();
-            board.drawGameboard();
+function respondToClick(e) {
+    if (board.setFieldValue(e.target.id)) {
+        let gameStatus = board.checkWin()
+        if (gameStatus === '') {
+            board.setNextPlayer();
+        } else if (gameStatus === "draw") {
+            disableEvents(fields);
+            // write a message of a draw to a HTML element
+            console.log("It's a draw.");
         } else {
-            console.log('Field already taken.');
+            disableEvents(fields);
+            // write a message of the winner to a HTML element
+            console.log('Player ' + board.getNextPlayer().symbol + ' wins');
         }
-    });
+        board.drawGameboard();
+    } else {
+        console.log('Field already taken.');
+    }
+}
+
+
+function disableEvents(fields) {
+    for (let i = 0; i < fields.length; i++) {
+        fields.item(i).removeEventListener('click', respondToClick);
+    }
+}
+
+for (let i = 0; i < fields.length; i++) {
+    fields.item(i).addEventListener('click', respondToClick);
 }
